3aabd253a7f083011a0198505646489dab09e0c3
wamtitu <wamtitujose@gmail.com>
2023-11-28 22:50:04 +0300

    more changes

--- data/config.js
+++ data/config.js
@@ -1,16 +1,3 @@
-import dotenv from 'dotenv'
-import assert from 'assert'
-
-
-dotenv.config();
-
-const{port,sqlServer,sqlUser,sqlPwd,sqlDb,JWT_SECRET} = process.env
-
-
-
-assert(port,'port is required');
-
-
 
 const config ={
     port: 5000,
